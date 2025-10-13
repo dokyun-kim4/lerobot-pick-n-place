75c02df5d4215d0b95e46f836f9b878375a56d45
dokyun-kim4 <donny5557@gmail.com>
2025-10-13 00:13:17 +0000

    feat(lerobot_control): implement controller launch file

---
+++ ros_ws/src/lerobot_control/launch/controller.launch.py
@@ -0,0 +1,53 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    pkg_share = get_package_share_directory("lerobot_description")
+    xacro_file = os.path.join(pkg_share, 'urdf', 'lerobot_new.xacro')
+    robot_description = ParameterValue(Command([
+        'xacro ', xacro_file
+        ]),
+        value_type= str
+    )
+    
+
+    robot_state_publisher = Node(
+                                    package='robot_state_publisher',
+                                    executable='robot_state_publisher',
+                                    parameters=[{'robot_description': robot_description, 'use_sim_time': True}]                 
+                                )
+    joint_state_broadcaster = Node(
+                                    package='controller_manager',
+                                    executable='spawner',
+                                    arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
+                                    parameters=[{'use_sim_time': True}]
+                                )   
+
+    arm_controller = Node(
+                            package='controller_manager',
+                            executable='spawner',
+                            arguments=['arm_controller', '--controller-manager', '/controller_manager'],
+                            parameters=[{'use_sim_time': True}]
+                        )
+
+    gripper_controller = Node(
+                            package='controller_manager',
+                            executable='spawner',
+                            arguments=['gripper_controller', '--controller-manager', '/controller_manager'],
+                            parameters=[{'use_sim_time': True}]
+                        )
+
+
+    return LaunchDescription(
+        [
+            robot_state_publisher,
+            joint_state_broadcaster,
+            arm_controller,
+            gripper_controller
+        ]
+    )       
