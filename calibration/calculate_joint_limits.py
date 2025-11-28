bafa285f4c48f2d7f82e3e8199cad5c6b14502f8
dokyun-kim4 <donny5557@gmail.com>
2025-11-28 19:05:04 +0000

    style: :art: apply ruff formatting

--- calibration/calculate_joint_limits.py
+++ calibration/calculate_joint_limits.py
@@ -2,9 +2,10 @@
 import json
 import math
 
-TPR = (4096/(2*math.pi)) # ticks per radian
+TPR = 4096 / (2 * math.pi)  # ticks per radian
 
-def calculate_joint_limits(calibration_file: str)->dict[str, tuple[float, float]]:
+
+def calculate_joint_limits(calibration_file: str) -> dict[str, tuple[float, float]]:
     """
     Calculate joint limits from calibration data.
 
@@ -13,16 +14,17 @@ def calculate_joint_limits(calibration_file: str)->dict[str, tuple[float, float]
     Returns:
         dict[str, tuple[float, float]]: A dictionary mapping joint names to their (min, max) limits in radians.
     """
-    
-    with open(calibration_file, 'r') as file:
+
+    with open(calibration_file, "r") as file:
         calibration_data = json.load(file)
 
     joint_limits = {}
     for idx, data in calibration_data.items():
-        angle_range = (data['range_max'] - data['range_min']) / TPR
-        joint_limits[idx] = (round(-angle_range/2, 2), round(angle_range/2, 2))
-        
+        angle_range = (data["range_max"] - data["range_min"]) / TPR
+        joint_limits[idx] = (round(-angle_range / 2, 2), round(angle_range / 2, 2))
+
     return joint_limits
 
+
 if __name__ == "__main__":
-    print(calculate_joint_limits('lerobot_calibration.json'))
+    print(calculate_joint_limits("lerobot_calibration.json"))
--- ros_ws/src/lerobot_control/launch/controller.launch.py
+++ ros_ws/src/lerobot_control/launch/controller.launch.py
@@ -7,14 +7,11 @@ from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 # Pre-define all controller nodes to launch using `controller_manager spawner`
-STARTUP_CONTROLLERS = [
-                        "joint_state_broadcaster",
-                        "gripper_controller",
-                        "arm_controller"
-                    ]
+STARTUP_CONTROLLERS = ["joint_state_broadcaster", "gripper_controller", "arm_controller"]
 
 LEROBOT_DESC_DIR = get_package_share_path("lerobot_description")
 LEROBOT_CTRL_DIR = get_package_share_path("lerobot_control")
+
 
 def build_nodes_runtime(context):
     usb_port = LaunchConfiguration("usb_port").perform(context)
@@ -23,30 +20,27 @@ def build_nodes_runtime(context):
     controller_config_file = LaunchConfiguration("controller_config_file").perform(context)
 
     # ---- Build xacro file ---- #
-    xacro_file = os.path.join(LEROBOT_DESC_DIR, 'urdf', 'lerobot.xacro')
+    xacro_file = os.path.join(LEROBOT_DESC_DIR, "urdf", "lerobot.xacro")
     xacro_args = {
         "ros2_control_file": ros2_control_xacro_file,
         "usb_port": usb_port,
-        "ros2_control_hardware_type": hardware_type
+        "ros2_control_hardware_type": hardware_type,
     }
-    
+
     cmd = ["xacro ", xacro_file]
     for key, value in xacro_args.items():
         cmd.append(f" {key}:={value}")
 
-    robot_description = ParameterValue(
-        Command(cmd),
-        value_type=str
-    )
+    robot_description = ParameterValue(Command(cmd), value_type=str)
 
     all_nodes = []
 
-    # ----- Set up robot_state_publisher node ----- #   
+    # ----- Set up robot_state_publisher node ----- #
     all_nodes.append(
         Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        parameters=[
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            parameters=[
                 {"robot_description": robot_description},
             ],
         )
@@ -72,7 +66,7 @@ def build_nodes_runtime(context):
                 arguments=[controller],
             )
         )
-    
+
     # ----- Rviz for visualization ----- #
     all_nodes.append(
         Node(
@@ -80,34 +74,31 @@ def build_nodes_runtime(context):
             executable="rviz2",
             name="rviz2",
             output="screen",
-            arguments=["-d", os.path.join(LEROBOT_DESC_DIR, "rviz", "display.rviz")]
-            )
+            arguments=["-d", os.path.join(LEROBOT_DESC_DIR, "rviz", "display.rviz")],
+        )
     )
 
     return all_nodes
 
 
 def generate_launch_description():
-
     # ----- Set up LaunchArguments ----- #
 
     usb_port_arg = DeclareLaunchArgument(
         "usb_port",
         default_value="/dev/LeRobot",
-        description="USB port for robot. Used when `hardware_type` is real"
+        description="USB port for robot. Used when `hardware_type` is real",
     )
 
     hardware_type_arg = DeclareLaunchArgument(
         "hardware_type",
         default_value="mock_components",
-        description="Hardware type for robot. Supported types: [mock_components, real]"
+        description="Hardware type for robot. Supported types: [mock_components, real]",
     )
 
     ros2_control_xacro_file_arg = DeclareLaunchArgument(
         "ros2_control_xacro_file",
-        default_value=os.path.join(
-            LEROBOT_DESC_DIR, "urdf", "lerobot_ros2_control.xacro"
-        ),
+        default_value=os.path.join(LEROBOT_DESC_DIR, "urdf", "lerobot_ros2_control.xacro"),
         description="Full path to the ros2_control xacro file",
     )
 
