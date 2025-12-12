f97845885b3e958b864e536f558ae61914e40e70
dokyun-kim4 <donny5557@gmail.com>
2025-12-12 04:09:01 +0000

    feat(lerobot_routine) Add support for adding gripper task

--- ros_ws/src/lerobot_routine/src/teach_routine.cpp
+++ ros_ws/src/lerobot_routine/src/teach_routine.cpp
@@ -2,6 +2,7 @@
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 
 #include <termios.h>
 #include <unistd.h>
@@ -19,13 +20,31 @@ class RoutineBuilder : public rclcpp::Node {
     // TF2 buffer and listener for arm pose
     tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
     tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+    
+    cb_group_ = this->create_callback_group(
+        rclcpp::CallbackGroupType::MutuallyExclusive);
+        rclcpp::SubscriptionOptions options;
+        options.callback_group = cb_group_;
 
+    jaw_subscriber_ = this->create_subscription<sensor_msgs::msg::JointState>(
+      "/joint_states", 10,
+      [this](const sensor_msgs::msg::JointState::SharedPtr msg) {
+        double jaw_joint_val = (msg->position)[5]; // get jaw joint value
+        if (!jaw_position_set_)
+        {
+          jaw_position_prev_ = jaw_joint_val;
+          jaw_position_set_ = true;
+        } else {
+          jaw_position_latest_ = jaw_joint_val; 
+        }
+      }, options);
+
+    // Run main loop 
     timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&RoutineBuilder::run, this));
-
     routine_vec_ = std::vector<Task>();
     
     RCLCPP_INFO(this->get_logger(), "Routine Teach Mode. Press `s` to save current position, `q` to export sequence to yaml.");
-    rclcpp::sleep_for(std::chrono::seconds(2));
+    rclcpp::sleep_for(std::chrono::seconds(1));
   }
   
   void run() {
@@ -51,7 +70,7 @@ class RoutineBuilder : public rclcpp::Node {
                 save_routine(routine_name_.c_str(), routine_vec_);
                 break;        
             case 's':
-                saveGripperPose();
+                savePoseAsTask();
                 break;
             }
 
@@ -66,24 +85,46 @@ class RoutineBuilder : public rclcpp::Node {
   }
 
  private:
+
   std::string routine_name_;
   std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
   std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
   std::vector<Task> routine_vec_;
+  bool jaw_position_set_ = false;
+  double jaw_position_latest_;
+  double jaw_position_prev_;
+  double JAW_DELTA_TOLERANCE = 0.01;
 
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr jaw_subscriber_;
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::CallbackGroup::SharedPtr cb_group_;
 
-  void saveGripperPose()
-  {
+  void savePoseAsTask()
+  { 
+    // Handle change in jaw joint position
+    if (abs(jaw_position_latest_ - jaw_position_prev_) > JAW_DELTA_TOLERANCE)
+    {
+      Task task = [](auto &jaw_position_latest_)
+      {
+        Task task;
+        task.target = "gripper";
+        task.gripper_position = jaw_position_latest_;
+        return task;
+      }(jaw_position_latest_);
+      jaw_position_prev_ = jaw_position_latest_;
+
+      RCLCPP_INFO(this->get_logger(), "Saved current jaw position as task: %.2f", jaw_position_latest_);
+      routine_vec_.push_back(task);
+    }
 
     geometry_msgs::msg::TransformStamped transformStamped;
     transformStamped = tf_buffer_->lookupTransform("base_link", "gripper", tf2::TimePointZero);
 
-    
-    Task task;
-    task.target = "arm";
-    task.pose = [](auto &transformStamped)
+    Task task = [](auto &transformStamped)
     {
+      Task task;
+      task.target = "arm";
+
       geometry_msgs::msg::Pose msg;
       msg.position.x = transformStamped.transform.translation.x;
       msg.position.y = transformStamped.transform.translation.y;
@@ -92,12 +133,14 @@ class RoutineBuilder : public rclcpp::Node {
       msg.orientation.y = transformStamped.transform.rotation.y;
       msg.orientation.z = transformStamped.transform.rotation.z;
       msg.orientation.w = transformStamped.transform.rotation.w;
-      return msg;
+
+      task.pose = msg;
+      return task;
     }(transformStamped);
 
     routine_vec_.push_back(task);
 
-    RCLCPP_INFO(this->get_logger(), "Captured current eef pose: [%.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f]", 
+    RCLCPP_INFO(this->get_logger(), "Saved current eef pose as task: [%.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f]", 
                 task.pose.position.x, task.pose.position.y, task.pose.position.z, task.pose.orientation.x, task.pose.orientation.y, task.pose.orientation.z, task.pose.orientation.w);
   }
 };
@@ -105,7 +148,10 @@ class RoutineBuilder : public rclcpp::Node {
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   auto routine_builder = std::make_shared<RoutineBuilder>();
-  routine_builder->run();
+  rclcpp::executors::MultiThreadedExecutor executor;
+
+  executor.add_node(routine_builder);
+  executor.spin();
   rclcpp::shutdown();
   return 0;
 }
