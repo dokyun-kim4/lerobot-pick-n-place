1b660c3969ee148d7de960c143594758be534d2f
Dominic Salmieri <dsalmieri@olin.edu>
2025-12-12 22:36:27 +0000

    Teleop working

--- ros_ws/src/lerobot_teleop/src/teleop.cpp
+++ ros_ws/src/lerobot_teleop/src/teleop.cpp
@@ -1,10 +1,14 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
 #include <trajectory_msgs/msg/joint_trajectory.hpp>
+#include <trajectory_msgs/msg/joint_trajectory_point.hpp>
 #include <termios.h>
 #include <unistd.h>
 
 using std::placeholders::_1;
+
+const float STEP = 0.01;
+const float TIME = 20000000;
 
 class KeyboardTeleop : public rclcpp::Node
 {
@@ -18,18 +22,17 @@ public:
         
         subscriber_ = this->create_subscription<sensor_msgs::msg::JointState>(
             "joint_states", 10, std::bind(&KeyboardTeleop::setJointState, this, _1), options);
-        publisher_ = this->create_publisher<trajectory_msgs::msg::JointTrajectory>("joint_trajectory", 10);
+        publisher_ = this->create_publisher<trajectory_msgs::msg::JointTrajectory>("/arm_controller/joint_trajectory", 10);
         RCLCPP_INFO(this->get_logger(), "Entering Keyboard Teleop Mode");
 
         timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&KeyboardTeleop::getInput, this), cb_group_);
 
-        JOINT_NAMES = {"shoulder_pan", "shoulder_lift", "elbow_flex", "wrist_flex", "wrist_roll", "gripper_joint"};
+        JOINT_NAMES = {"shoulder_pan", "shoulder_lift", "elbow_flex", "wrist_flex", "wrist_roll"};
     }
 
 private:
     void setJointState(const sensor_msgs::msg::JointState::SharedPtr state)
     {
-        RCLCPP_INFO(this->get_logger(), "Joint states lol");
         this->joint_states_ = *state;
     }
 
@@ -52,6 +55,11 @@ private:
             { // Read one character
                 std::cout << "Key pressed: " << c << std::endl;
 
+                std::vector<double> joint_angles;
+
+                joint_angles = this->joint_states_.position;
+                joint_angles.pop_back(); // Remove the gripper joint angle
+
                 switch (c)
                 {
                 case 'l':
@@ -60,11 +68,53 @@ private:
                 case 'p':
                     std::cout << joint_states_.position[0] << std::endl;
                     break;
+                case 'q':
+                    joint_angles[0] += STEP;
+                    break;
+                case 'a':
+                    joint_angles[0] -= STEP;
+                    break;
+                case 'w':
+                    joint_angles[1] += STEP;
+                    break;
+                case 's':
+                    joint_angles[1] -= STEP;
+                    break;
+                case 'e':
+                    joint_angles[2] += STEP;
+                    break;
+                case 'd':
+                    joint_angles[2] -= STEP;
+                    break;
+                case 'r':
+                    joint_angles[3] += STEP;
+                    break;
+                case 'f':
+                    joint_angles[3] -= STEP;
+                    break;
+                case 't':
+                    joint_angles[4] += STEP;
+                    break;
+                case 'g':
+                    joint_angles[4] -= STEP;
+                    break;
                 }
 
                 if (!rclcpp::ok() || !running) {
                     break;
                 }
+
+                trajectory_msgs::msg::JointTrajectory joint_trajectory;
+                joint_trajectory.header.stamp = this->get_clock()->now();
+                joint_trajectory.header.frame_id = "base_link";
+                joint_trajectory.joint_names = JOINT_NAMES;
+
+                trajectory_msgs::msg::JointTrajectoryPoint point;
+                point.positions = joint_angles;
+                point.time_from_start = rclcpp::Duration(0, TIME);
+                joint_trajectory.points.push_back(point);
+
+                publisher_->publish(joint_trajectory);
             }
         }
 
